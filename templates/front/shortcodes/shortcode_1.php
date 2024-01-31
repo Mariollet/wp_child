be8d90621ad6557b35a07423acdb0fa6adc5f97f
Mariollet <mariollet@outlook.fr>
2024-02-01 00:41:42 +0100

    format document

--- templates/front/shortcodes/shortcode_1.php
+++ templates/front/shortcodes/shortcode_1.php
@@ -1,5 +1,5 @@
 <?php
-  wp_enqueue_style('wp_shortcode_1_css', get_stylesheet_directory_uri() . '/assets/css/shortcode_1.css');
+  wp_enqueue_style('wp_shortcode_1_css', get_stylesheet_directory_uri() . '/assets/css/shortcode/shortcode_1.css');
   wp_enqueue_script('wp_shortcode_1_js', get_stylesheet_directory_uri() . '/assets/js/shortcode/shortcode_1.js', ['jquery']);
 ?>
 
